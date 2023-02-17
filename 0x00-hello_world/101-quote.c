58fc95c8fd32303f528e5c7b1f1f4907ae29cf50
zulutshangisa <sandlasenkosi173@gmail.com>
2023-02-16 18:52:58 -0800

    adds

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char person[] = "Dora Korpar";
+	char date[] = "2015-10-19";
+
+	printf("and that piece of art is useful\" - %s, %s\n", person, date);
+	return (1);
+}
